2d57a3e9e8ee46fc476e522b014070c3dae055e1
quocgiahcmut <duonggia31@gmail.com>
2022-04-12 15:44:11 +0700

    change command properties name

--- WebApi/GlobalUsing.cs
+++ WebApi/GlobalUsing.cs
@@ -5,6 +5,15 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.SignalR;
 global using Microsoft.EntityFrameworkCore;
 
+global using SparkplugNet;
+global using SparkplugNet.Core;
+global using SparkplugNet.Core.Application;
+global using SparkplugNet.Custom;
+global using VersionB = SparkplugNet.VersionB;
+global using SparkplugNet.VersionB.Data;
+
+global using Serilog;
+
 global using Application;
 global using Application.Commands.Nodes;
 global using Application.Commands.Devices;
